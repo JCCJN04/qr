6ae67d8eee56ccdfd043fa0303a5ad330f50a28d
Juan Carlos Mendoza Castillo <mendozacastillo387@gmail.com>
2024-07-30 13:48:03 -0600

    Actualizar proyecto con la nueva versión

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployeeList from './components/EmployeeList';
 import EmployeeDetail from './components/EmployeeDetail';
 import AdminPanel from './components/AdminPanel';
-import AddEmployeeForm from './components/AddEmployeeForm'; // Importa el componente AddEmployeeForm
-import EditEmployeeForm from './components/EditEmployeeForm'; // Importa el componente EditEmployeeForm
-import EmployeeCredential from './components/EmployeeCredential'; // Importa el componente EmployeeCredential
+import AddEmployeeForm from './components/AddEmployeeForm';
+import EditEmployeeForm from './components/EditEmployeeForm';
+import EmployeeCredential from './components/EmployeeCredential';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/credenciales">
       <Routes>
         <Route path="/" element={<EmployeeList />} />
         <Route path="/empleados/:id" element={<EmployeeDetail />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/credenciales" element={<EmployeeList />} />
-        <Route path="/add-employee" element={<AddEmployeeForm />} /> {/* Nueva ruta para agregar empleados */}
-        <Route path="/edit-employee/:id" element={<EditEmployeeForm />} /> {/* Nueva ruta para editar empleados */}
-        <Route path="/empleado/:num_empleado" element={<EmployeeCredential />} /> {/* Ruta para credenciales usando num_empleado */}
+        <Route path="/add-employee" element={<AddEmployeeForm />} />
+        <Route path="/edit-employee/:id" element={<EditEmployeeForm />} />
+        <Route path="/empleado/:num_empleado" element={<EmployeeCredential />} />
       </Routes>
     </Router>
   );
